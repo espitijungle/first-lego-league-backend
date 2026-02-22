cfc265d00f3bb72243b8ab78e59c64d81f07dda5
espitijungle <lorcamulerosergio@gmail.com>
2026-02-22 11:00:32 +0100

    Fix lombok, need one by one

--- src/main/java/cat/udl/eps/softarch/demo/domain/ScientificProject.java
+++ src/main/java/cat/udl/eps/softarch/demo/domain/ScientificProject.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
